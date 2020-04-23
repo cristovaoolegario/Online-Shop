2faaad2893e7dd900add4fec014fc51b5e346e0a
Cristóvão <cristovaoolegario@gmail.com>
2020-04-23 19:06:37 -0300

    Adding Database project

---
+++ Shop.Database/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Models;
+
+namespace Shop.Database
+{
+  public class ApplicationDbContext : IdentityDbContext
+  {
+    protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+  }
+}
