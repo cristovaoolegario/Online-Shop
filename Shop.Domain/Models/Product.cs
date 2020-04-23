8eb041d1baacb5c1d8a1f1b1620e1207daaf4b27
Cristóvão <cristovaoolegario@gmail.com>
2020-04-23 19:00:07 -0300

    Create Product domain base class.

--- Shop.Domain/Models/Product.cs
+++ Shop.Domain/Models/Product.cs
@@ -6,5 +6,9 @@ namespace Shop.Domain.Models
 {
   public class Product
   {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Value { get; set; }
   }
 }
