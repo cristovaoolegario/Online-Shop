49415aae0d5cf0fc0db8d9f6ec8d679ae67cf21d
Cristóvão <cristovaoolegario@gmail.com>
2020-04-23 19:06:52 -0300

    Update Create product.

--- Shop.Application/Products/CreateProduct.cs
+++ Shop.Application/Products/CreateProduct.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Shop.Database;
+using Shop.Domain.Models;
 
 namespace Shop.Application.Products
 {
   public class CreateProduct
   {
+    private ApplicationDbContext _context;
+    public CreateProduct(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public void Do(int id, string name, string description)
+    {
+      _context.Products.Add(new Product
+      {
+        Id = id,
+        Name = name,
+        Description = description
+      });
+    }
   }
 }
