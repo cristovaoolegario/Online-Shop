e0f2bc45a6ed7581e2eda3fbe230a6941404a45f
Cristóvão <cristovaoolegario@gmail.com>
2020-05-05 00:02:53 -0300

    Updating Product's domain model to contain a collection of 'OrderProducts'.'
    It's necessary to generate an n-n relation with ef core.

--- Shop.Domain/Models/Product.cs
+++ Shop.Domain/Models/Product.cs
@@ -12,5 +12,6 @@ namespace Shop.Domain.Models
     public decimal Value { get; set; }
 
     public ICollection<Stock> Stock { get; set; }
+    public ICollection<OrderProduct> OrderProducts { get; set; }
   }
 }
