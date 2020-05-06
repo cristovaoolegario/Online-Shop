b12c8bb167b77d0a3238823d0d3509ec322580ac
Cristóvão <cristovaoolegario@gmail.com>
2020-05-06 01:07:36 -0300

    Adding 'delete product' feature.
    - Changing DeleteProduct file to ProductsAdmin folder.
    - Implementing delete a product by id feature.

---
+++ Shop.Application/ProductsAdmin/DeleteProduct.cs
@@ -0,0 +1,26 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Application.ProductsAdmin
+{
+  public class DeleteProduct
+  {
+    private ApplicationDbContext _context;
+    public DeleteProduct(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Do(int id)
+    {
+      var Product = _context.Products.
+                             FirstOrDefault(item => item.Id == id);
+
+      _context.Products.Remove(Product);
+
+      await _context.SaveChangesAsync();
+    }
+  }
+}
