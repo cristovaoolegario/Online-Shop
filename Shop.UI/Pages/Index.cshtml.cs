168eaecdaa21e1cf60241582b1b55eae07cfe81b
Cristóvão <cristovaoolegario@gmail.com>
2020-05-06 01:28:28 -0300

    Removing unnecessary implementations from Index file.

--- Shop.UI/Pages/Index.cshtml.cs
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Shop.Application.Products;
-using Shop.Application.ProductsAdmin;
 using Shop.Database;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -17,22 +16,11 @@ namespace Shop.UI.Pages
       _context = dbContext;
     }
 
-    [BindProperty]
-    public CreateProduct.ProductViewModel Product { get; set; }
-
     public IEnumerable<GetProducts.ProductViewModel> Products { get; set; }
 
     public void OnGet()
     {
       Products = new GetProducts(_context).Do();
-    }
-
-    public async Task<IActionResult> OnPost()
-    {
-      await new CreateProduct(_context).
-        Do(Product);
-
-      return RedirectToPage("Index");
     }
   }
 }
