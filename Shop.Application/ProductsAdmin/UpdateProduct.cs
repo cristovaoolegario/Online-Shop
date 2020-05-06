f2f5a1fe90e964e44044a19e2ac6cbd594702d09
Cristóvão <cristovaoolegario@gmail.com>
2020-05-06 01:27:51 -0300

    Adding 'update product' feature.
    - Changing UpdateProduct file to ProductsAdmin folder.
    - Implementing update product feature.

---
+++ Shop.Application/ProductsAdmin/UpdateProduct.cs
@@ -0,0 +1,27 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System.Threading.Tasks;
+
+namespace Shop.Application.ProductsAdmin
+{
+  public class UpdateProduct
+  {
+    private ApplicationDbContext _context;
+    public UpdateProduct(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public async Task Do(ProductViewModel viewModel)
+    {
+      await _context.SaveChangesAsync();
+    }
+
+    public class ProductViewModel
+    {
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public decimal Value { get; set; }
+    }
+  }
+}
