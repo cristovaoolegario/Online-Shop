147f5d32e57bbe8988f866b76e29b4231be24088
Cristóvão <cristovaoolegario@gmail.com>
2020-05-06 01:29:45 -0300

    Adding 'Get product by id' feature.
    - Adding GetProduct file to ProductsAdmin folder.
    - Implementing get by id product feature.

---
+++ Shop.Application/ProductsAdmin/GetProduct.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Database;
+using System.Linq;
+
+namespace Shop.Application.ProductsAdmin
+{
+  public class GetProduct
+  {
+    private ApplicationDbContext _context;
+
+    public GetProduct(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public ProductViewModel Do(int id) =>
+      _context.Products.Where(item => item.Id == id).
+                        Select(item => new ProductViewModel()
+                        {
+                          Id = item.Id,
+                          Name = item.Name,
+                          Description = item.Description,
+                          Value = item.Value
+                        }).
+                        FirstOrDefault();
+
+    public class ProductViewModel
+    {
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public decimal Value { get; set; }
+    }
+  }
+}
