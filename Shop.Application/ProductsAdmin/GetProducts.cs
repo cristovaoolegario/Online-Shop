84de3d936fc5c9dc39b2ebd7ef0c636563e0ee38
Cristóvão <cristovaoolegario@gmail.com>
2020-05-06 01:03:39 -0300

    Adding GetProducts to ProductsAdmin's folder.

---
+++ Shop.Application/ProductsAdmin/GetProducts.cs
@@ -0,0 +1,38 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.ProductsAdmin
+{
+  public class GetProducts
+  {
+    private ApplicationDbContext _context;
+
+    public GetProducts(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public IEnumerable<ProductViewModel> Do() =>
+      _context.Products.
+               ToList().
+               Select(item => new ProductViewModel()
+               {
+                 Id = item.Id,
+                 Name = item.Name,
+                 Description = item.Description,
+                 Value = item.Value
+               });
+
+    public class ProductViewModel
+    {
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public decimal Value { get; set; }
+    }
+
+  }
+}
